97a5c8a9f6b85a3ff43fdb14c9be8d6a0cb051ef
Haoliang Han <598510172@qq.com>
2025-01-12 22:38:09 +0800

    修改

--- assets/javascripts/discourse/api-initializers/hide-title.js
+++ assets/javascripts/discourse/api-initializers/hide-title.js
@@ -9,21 +9,23 @@ export default {
         pluginId: "hide-topic-title",
         didInsertElement() {
           this._super(...arguments); // 调用原始的 didInsertElement 方法
-
+          console.log('topic-进入插件内部')
           const title = this.element.querySelector(".topic-title");
 
-          if(/t-bot-\d{13,13}/.test(title.text)){
+          if(/t-bot-\d{13,13}/.test(title.innerText)){
+            console.log('topic-插件匹配通过')
             title.style.display = "none";
           }
         },
       });
 
       api.onPageChange(() => {
-        console.log(1111)
+        console.log('topic-detail-进入插件内部')
         // 判断是否是主题详情页
         if (window.location.pathname.includes("/t/")) {
           const topicTitle = document.querySelector(".title-wrapper h1");
-          if(/t-bot-\d{13,13}/.test(topicTitle.text)){
+          if(/t-bot-\d{13,13}/.test(topicTitle.innerText)){
+            console.log('topic-detail-插件匹配通过')
             topicTitle.style.display = "none"; // 隐藏标题
           }
         }
